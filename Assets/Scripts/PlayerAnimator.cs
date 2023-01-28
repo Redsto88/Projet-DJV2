f2504f82a590c3c956ab15dc9a39b86a9a3d95a1
benjamin_farigoul <ben.farigoul@gmail.com>
2023-01-28 15:15:49 +0100

    Début d'animator

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator _animator;
+
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO Centraliser les inputs dans un script à part pour faire plus propre avec le PlayerController
+        Vector3 direction = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        float speedParam = Mathf.Clamp01(direction.magnitude);
+        _animator.SetFloat(Speed, speedParam);
+    }
+}
