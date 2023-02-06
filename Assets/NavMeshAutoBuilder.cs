baca2e712e56fee32928f672f3a834d01a54942f
Redsto88 <redsto88@gmail.com>
2023-02-06 17:23:55 +0100

    [CU-860pv02kg] NavMeshBuilder

---
+++ Assets/NavMeshAutoBuilder.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAutoBuilder : MonoBehaviour
+{
+    protected void Awake()
+    {
+        var markups = new List<NavMeshBuildMarkup>();
+        var sources = new List<NavMeshBuildSource>();
+
+
+        var markup = new NavMeshBuildMarkup();
+        markup.root = transform;
+        markup.overrideArea = true;
+        markup.area = NavMesh.GetAreaFromName("Walkable");
+
+        NavMeshBuilder.CollectSources(transform,
+            LayerMask.GetMask("Ground"),
+            NavMeshCollectGeometry.PhysicsColliders,
+            NavMesh.GetAreaFromName("Walkable"),
+            markups,
+            sources);
+        
+        var settings = NavMesh.GetSettingsByIndex(0);
+        var settings2 = new NavMeshBuildSettings();
+        settings2.agentTypeID = 0;
+        settings2.agentRadius = 0.5f;
+        settings2.agentHeight = 2f;
+        settings2.agentSlope = 45f;
+        settings2.agentClimb = 4f;
+
+
+        var data = NavMeshBuilder.BuildNavMeshData(
+            settings2,
+            sources,
+            new Bounds(
+                Vector3.zero,
+                new Vector3(50f, 50f, 50f)),
+            transform.position,
+            Quaternion.identity);
+
+        NavMesh.AddNavMeshData(data);
+    }
+}
