d3df2ecc58d0cd00e52322052e37a7bb2549b29d
benjamin_farigoul <ben.farigoul@gmail.com>
2023-03-03 23:15:51 +0100

    Debug ennemis

--- Assets/Scripts/Collectables/HealCollectable.cs
+++ Assets/Scripts/Collectables/HealCollectable.cs
@@ -9,16 +9,19 @@ public class HealCollectable : ACollectable
 
     protected override void OnCollect()
     {
+        print("heal");
         PlayerManager.Instance.ApplyDamaged(-healAmount);
         StartCoroutine(OnCollectCoroutine());
     }
 
     protected override void OnTriggerEnter(Collider other)
     {
+        print("trigger enter");
         if (other.TryGetComponent(out PlayerManager pC))
         {
             if (pC.IsFullHealth())
             {
+                print("player is full life");
                 return;
             }
             OnCollect();
