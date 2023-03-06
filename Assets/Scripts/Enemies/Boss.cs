2efd3392e7a6c73bd615b4e008796d5c1bf64d74
benjamin_farigoul <ben.farigoul@gmail.com>
2023-03-06 09:11:51 +0100

    push enigme

--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -2,36 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class DistanceEnemyBehaviour : BasicEnemyBehaviour
+public class Boss : DistanceEnemyBehaviour
 {
-
-    [SerializeField] private GameObject _bulletPrefab;
-
-    public float _cooldown = 2f;
-    public float _cooldownTimer = 0f;
-    
-    
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
+        transform.LookAt(PlayerController.Instance.transform);
         if (!portalFlag && !_target.IsUnityNull())
         {
             if ((transform.position - _target.position).magnitude > navMeshAgent.stoppingDistance)
             {
                 navMeshAgent.destination = _target.position;
             }
+            
+            else if ((transform.position - _target.position).magnitude < navMeshAgent.stoppingDistance - 4)
+            {
+                
+            }
+            
             else
             {
                 navMeshAgent.destination = transform.position;
                 //rotate to target
-                Vector3 targetDir = _target.position - transform.position;
+                /*Vector3 targetDir = _target.position - transform.position;
                 float step = 5 * Time.deltaTime;
                 Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
-                transform.rotation = Quaternion.LookRotation(newDir);
+                transform.rotation = Quaternion.LookRotation(newDir);*/
                 
             }
             if(_cooldownTimer >= _cooldown)
@@ -41,20 +44,5 @@ public class DistanceEnemyBehaviour : BasicEnemyBehaviour
             }
             else _cooldownTimer += Time.deltaTime;
         }
-
     }
-
-    protected void Shoot()
-    {
-        if (portalFlag)
-        {
-            return;
-        }
-        
-        GameObject bullet = Instantiate(_bulletPrefab, transform.position + transform.forward*1.5f, transform.rotation);
-        bullet.transform.position = new Vector3(bullet.transform.position.x, 1f, bullet.transform.position.z);
-        bullet.GetComponent<Bullet>().target = _target;
-        bullet.gameObject.SetActive(true);
-    }
-    
 }
